395a60f645ddbc761d8e085bd37dc55f417afba8
suredammy <suredammy@gmail.com>
2020-12-21 08:53:44 -0600

    Added new files

--- mpl_square.py
+++ mpl_square.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 
 squares = [x ** 2 for x in range(50, 100)]
 
+plt.style.use('bmh') #Use builtin styles. view with print(plt.style.available)
 fig, ax = plt.subplots()
 ax.plot(squares, linewidth = 4)
 
 
 
 #Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize= 32)
+ax.set_title("Square Numbers", fontsize= 24)
 ax.set_xlabel("Value", fontsize = 16)
 ax.set_ylabel("Square of Values", fontsize = 16)
 
